524fd6356e4d3803195e03fe4b0227ea486b1635
_running_program <129178005+KaReeeeeeeeEM@users.noreply.github.com>
2024-06-16 11:07:27 +0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -127,13 +127,13 @@ export default function Dashboard() {
         setLoading(true);
   
         // Fetch popular movies (5 pages)
-        const popularMoviesData = await fetchMoviesByCategory("popular", 5);
+        const popularMoviesData = await fetchMoviesByCategory("movie/popular", 5);
   
         // Fetch trending movies (5 pages)
-        const trendingMoviesData = await fetchMoviesByCategory("trending", 5);
+        const trendingMoviesData = await fetchMoviesByCategory("trending/all/day", 5);
   
         // Fetch upcoming movies (5 pages)
-        const upcomingMoviesData = await fetchMoviesByCategory("upcoming", 5);
+        const upcomingMoviesData = await fetchMoviesByCategory("movie/upcoming", 5);
   
         // Update state with fetched data
         setPopularMovies(popularMoviesData);
@@ -156,7 +156,7 @@ export default function Dashboard() {
   
       for (let page = 1; page <= pageCount; page++) {
         const response = await axios.get(
-          `https://api.themoviedb.org/3/movie/${category}?api_key=035c0f1a7347b310a5b95929826fc81f&language=en-US&page=${page}`
+          `https://api.themoviedb.org/3/${category}?api_key=035c0f1a7347b310a5b95929826fc81f&language=en-US&page=${page}`
         );
         const moviesData = response.data.results;
         allMovies = [...allMovies, ...moviesData];
@@ -241,8 +241,7 @@ export default function Dashboard() {
                     <IconButton>
                       <TrendingUp fontSize="large" />
                     </IconButton>
-                    <span style={{ color: "orange" }}>Trending</span> Movies &
-                    Shows
+                    <span style={{ color: "orange" }}>Featured</span> Content
                   </Typography>
                   <Grid
                     container
